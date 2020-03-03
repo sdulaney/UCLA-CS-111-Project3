dc7959756cc43ee8308605ff6401a84c97e07c96
psamandi <pari.samandi@gmail.com>
2020-03-03 15:30:10 -0800

    List of needed functions

--- proj3a/proj3a/lab3a.c
+++ proj3a/proj3a/lab3a.c
@@ -2,141 +2,197 @@
   NAME: Stewart Dulaney
   EMAIL: sdulaney@ucla.edu
   ID: 904-064-791
+
+  NAME: Pariya Samandi
+  EMAIL: pari.samandi@gmail.com
+  ID: 205-092-357
  */
 
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <getopt.h>
-
 #include <string.h>
-
 #include <errno.h>
-
 #include <pthread.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <math.h>
 
-#include<signal.h>
+//offset of 1024 bytes from the start of the device (from TA slide)
+#define SUPER_BLOCK_OFFSET 1024
+#define SUPER_BLOCK_SIZE 1024
+#define INODE_BLOCK_POINTER_WIDTH 4
+#define GROUP_DESCRIPTOR_TABLE_SIZE 32
+#define INODE_SIZE 128
 
-int num_threads = 0;
-int num_iterations = 0;
-int num_lists = 0;
-int opt_yield = 0;
-int opt_sync = 0;
-char * arg_sync = NULL;
-SortedListElement_t * element_arr = NULL;
-char ** key_arr = NULL;
-SortedList_t* list_arr = NULL;
-pthread_mutex_t* lock_arr = NULL;
-int* spin_lock_arr = NULL;
+struct super_t
+{
+};
 
-int main(int argc, char ** argv) {
+struct group_t
+{
+};
 
-    // Process all arguments
-    int c;
-    int opt_threads = 0;
-    int opt_iterations = 0;
-    int opt_lists = 0;
-    char * arg_threads = NULL;
-    char * arg_iterations = NULL;
-    char * arg_yield = NULL;
-    char * arg_lists = NULL;
-    char default_val = '1';
-
-    while (1) {
-        int option_index = 0;
-        static struct option long_options[] = {
-            {
-                "threads",
-                optional_argument,
-                0,
-                0
-            },
-            {
-                "iterations",
-                optional_argument,
-                0,
-                0
-            },
-            {
-                "yield",
-                required_argument,
-                0,
-                0
-            },
-            {
-                "sync",
-                required_argument,
-                0,
-                0
-            },
-	    {
-                "lists",
-                required_argument,
-                0,
-                0
-            },
-            {
-                0,
-                0,
-                0,
-                0
-            }
-        };
-
-        c = getopt_long(argc, argv, "t::i::y:s:l:",
-            long_options, & option_index);
-        if (c == -1)
-            break;
-
-        const char * name = long_options[option_index].name;
-        switch (c) {
-        case 0:
-            if (strcmp(name, "threads") == 0) {
-                opt_threads = 1;
-                if (optarg)
-                    arg_threads = optarg;
-                else
-                    arg_threads = & default_val;
-            } else if (strcmp(name, "iterations") == 0) {
-                opt_iterations = 1;
-                if (optarg)
-                    arg_iterations = optarg;
-                else
-                    arg_iterations = & default_val;
-            } else if (strcmp(name, "sync") == 0) {
-                opt_sync = 1;
-                if (optarg)
-                    arg_sync = optarg;
-            } else if (strcmp(name, "yield") == 0) {
-                if (optarg) {
-                    arg_yield = optarg;
-                    int len = strlen(arg_yield);
-                    for (int i = 0; i < len; i++) {
-                        if (optarg[i] == 'i')
-                            opt_yield |= INSERT_YIELD;
-                        else if (optarg[i] == 'd')
-                            opt_yield |= DELETE_YIELD;
-                        else if (optarg[i] == 'l')
-                            opt_yield |= LOOKUP_YIELD;
-                    }
-                }
-            } else if (strcmp(name, "lists") == 0) {
-                opt_lists = 1;
-                if (optarg)
-                    arg_lists = optarg;
-            }
-            break;
-
-        case '?':
-            fprintf(stderr, "usage: ./lab2_list [OPTION]...\nvalid options: --threads=# (default 1), --iterations=# (default 1), --yield=[idl], --sync=[ms], --lists=# (default 1)\n");
-            exit(1);
-            break;
-
-        default:
-            printf("?? getopt returned character code 0%o ??\n", c);
-        }
-    }
-
-    exit(0);
+void parsing_arg(int argc, char **argv)
+{
 }
+
+void alloc_mem()
+{
+}
+
+void parsing_SuperB()
+{
+}
+
+void parsing_GroupB()
+{
+}
+
+void parsing_bitM()
+{
+}
+
+void parsing_inode()
+{
+}
+
+void parsing_dir()
+{
+}
+
+void parsing_inDir()
+{
+}
+
+int main()
+{
+}
+
+// int num_threads = 0;
+// int num_iterations = 0;
+// int num_lists = 0;
+// int opt_yield = 0;
+// int opt_sync = 0;
+// char *arg_sync = NULL;
+// SortedListElement_t *element_arr = NULL;
+// char **key_arr = NULL;
+// SortedList_t *list_arr = NULL;
+// pthread_mutex_t *lock_arr = NULL;
+// int *spin_lock_arr = NULL;
+
+// int main(int argc, char **argv)
+// {
+
+//     // Process all arguments
+//     int c;
+//     int opt_threads = 0;
+//     int opt_iterations = 0;
+//     int opt_lists = 0;
+//     char *arg_threads = NULL;
+//     char *arg_iterations = NULL;
+//     char *arg_yield = NULL;
+//     char *arg_lists = NULL;
+//     char default_val = '1';
+
+//     while (1)
+//     {
+//         int option_index = 0;
+//         static struct option long_options[] = {
+//             {"threads",
+//              optional_argument,
+//              0,
+//              0},
+//             {"iterations",
+//              optional_argument,
+//              0,
+//              0},
+//             {"yield",
+//              required_argument,
+//              0,
+//              0},
+//             {"sync",
+//              required_argument,
+//              0,
+//              0},
+//             {"lists",
+//              required_argument,
+//              0,
+//              0},
+//             {0,
+//              0,
+//              0,
+//              0}};
+
+//         c = getopt_long(argc, argv, "t::i::y:s:l:",
+//                         long_options, &option_index);
+//         if (c == -1)
+//             break;
+
+//         const char *name = long_options[option_index].name;
+//         switch (c)
+//         {
+//         case 0:
+//             if (strcmp(name, "threads") == 0)
+//             {
+//                 opt_threads = 1;
+//                 if (optarg)
+//                     arg_threads = optarg;
+//                 else
+//                     arg_threads = &default_val;
+//             }
+//             else if (strcmp(name, "iterations") == 0)
+//             {
+//                 opt_iterations = 1;
+//                 if (optarg)
+//                     arg_iterations = optarg;
+//                 else
+//                     arg_iterations = &default_val;
+//             }
+//             else if (strcmp(name, "sync") == 0)
+//             {
+//                 opt_sync = 1;
+//                 if (optarg)
+//                     arg_sync = optarg;
+//             }
+//             else if (strcmp(name, "yield") == 0)
+//             {
+//                 if (optarg)
+//                 {
+//                     arg_yield = optarg;
+//                     int len = strlen(arg_yield);
+//                     for (int i = 0; i < len; i++)
+//                     {
+//                         if (optarg[i] == 'i')
+//                             opt_yield |= INSERT_YIELD;
+//                         else if (optarg[i] == 'd')
+//                             opt_yield |= DELETE_YIELD;
+//                         else if (optarg[i] == 'l')
+//                             opt_yield |= LOOKUP_YIELD;
+//                     }
+//                 }
+//             }
+//             else if (strcmp(name, "lists") == 0)
+//             {
+//                 opt_lists = 1;
+//                 if (optarg)
+//                     arg_lists = optarg;
+//             }
+//             break;
+
+//         case '?':
+//             fprintf(stderr, "usage: ./lab2_list [OPTION]...\nvalid options: --threads=# (default 1), --iterations=# (default 1), --yield=[idl], --sync=[ms], --lists=# (default 1)\n");
+//             exit(1);
+//             break;
+
+//         default:
+//             printf("?? getopt returned character code 0%o ??\n", c);
+//         }
+//     }
+
+//     exit(0);
+// }
